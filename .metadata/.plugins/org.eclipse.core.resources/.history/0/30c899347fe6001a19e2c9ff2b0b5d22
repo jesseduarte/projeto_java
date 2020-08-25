99222a5bd1264ad2ed6ea23a395cf5c60c2d930b
jesseduarte <jesse__duarte@hotmail.com>
2020-08-25 00:04:14 -0300

    Exercicios Java part I

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/0/30c899347fe6001a19e2c9ff2b0b5d22
@@ -0,0 +1,8 @@
+package data;
+
+public class Data {
+
+	public int dia;
+	public int mes;
+	public int ano;
+}
---
+++ exercicios/src/classe/Produto.java
@@ -0,0 +1,8 @@
+package classe;
+
+public class Produto {
+	
+	public String nome;
+	public Double preco;
+	public Double desconto;
+}
